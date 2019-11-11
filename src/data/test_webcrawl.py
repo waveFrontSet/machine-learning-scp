bfd19f8a3fdb66316294671dbdad3db8bb12021f
waveFrontSet <paul.bubenzer@gmail.com>
2019-11-11 14:22:50 +0100

    Added a test for the crawl_for function with mocks

--- src/data/test_webcrawl.py
+++ src/data/test_webcrawl.py
@@ -1,11 +1,14 @@
 import pytest
 import io
+from unittest.mock import Mock, mock_open, patch
+from click.testing import CliRunner
 from bs4 import BeautifulSoup
 from .webcrawl import (
     construct_url,
     filter_for_page_content,
     split_into_label_and_text,
     write_to,
+    crawl_for,
 )
 
 
@@ -16,9 +19,13 @@ TEST_PAGE = """
     </head>
     <body>
     <div id="page-content">
+        <p><strong>Object Class:</strong> Safe</p>
         <p>Some paragraph.</p>
     </div></body></html>
     """
+
+response_mock = Mock()
+response_mock.text = TEST_PAGE
 
 
 @pytest.mark.parametrize(
@@ -37,6 +44,7 @@ def test_construct_url(test_input, expected):
 def test_filter_for_page_content():
     expected = """
 <div id="page-content">
+<p><strong>Object Class:</strong> Safe</p>
 <p>Some paragraph.</p>
 </div>
     """.strip()
@@ -84,3 +92,14 @@ def test_write_to():
     assert label == lines[0]
     for i, p in enumerate(expected):
         assert p == lines[i + 1]
+
+
+@patch("requests.get", side_effect=lambda *args, **kwargs: response_mock)
+@patch("builtins.open", side_effect=mock_open)
+def test_crawl_for(mock_file, mock_requests):
+    runner = CliRunner()
+    result = runner.invoke(crawl_for, ["--lower", "2", "--upper", "3", "data/raw/"])
+    print(result.output)
+    print(result.exception)
+    mock_requests.assert_called_once_with("http://scp-wiki.net/scp-002")
+    mock_file.assert_called_once_with("data/raw/scp-002.txt", "w")
